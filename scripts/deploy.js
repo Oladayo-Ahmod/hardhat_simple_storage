189a4cad371ab2c1f71e756926d6df387725b0be
oladayo ahmod <oladayoahmod112@gmail.com>
2022-07-02 20:11:44 +0100

    verifying contract deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,7 +8,8 @@ async function main() {
   console.log(`deployed to ${deployContract.address}`)
   console.log(network.config)
   if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
-   await verify(deployContract.address,[])
+      await deployContract.deployTransaction.wait(6)
+      await verify(deployContract.address,[])
   }
 }
 
