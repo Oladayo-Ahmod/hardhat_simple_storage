914ef5406d691fac107db7054d7d71cfe150baf6
oladayo ahmod <oladayoahmod112@gmail.com>
2022-07-02 18:21:33 +0100

    verifying contract deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,29 @@
-const {ethers} = require('hardhat')
+const {ethers, run} = require('hardhat')
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage')
   const deployContract = await SimpleStorageFactory.deploy()
   console.log('deploying contract ')
   await deployContract.deployed()
-  console.log('deployed')
+  console.log(`deployed to ${deployContract.address}`)
 }
 
+async function verify(contractAddress, arg){
+  console.log('verifying ..')
+  try {
+      await run('verify:verify',{
+        address : contractAddress,
+        constructorArguments : arg
+      })
+  } catch (error) {
+    if (error.includes('already verified')) {
+      console.log('Already verifies')
+    }
+    else{
+      console.log(error)
+    }
+  }
+}
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
