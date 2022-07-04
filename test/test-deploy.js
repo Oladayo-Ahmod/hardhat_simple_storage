5a57aafc50f9a24d309145f43771491d17db34b0
oladayo ahmod <oladayoahmod112@gmail.com>
2022-07-04 17:12:07 +0100

    fix test bug

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -4,12 +4,13 @@ const {assert, expect} = require('chai')
 describe("SimpleStorage", ()=>{
     let SimpleStorage, SimpleStorageFactory
     beforeEach(async function(){
-        SimpleStorage = await ethers.getContractFactory('SimpleStorage')
-        SimpleStorageFactory = await SimpleStorage.deploy()
+        SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage')
+        SimpleStorage = await SimpleStorageFactory.deploy()
+
     })
     it("Current number should return 0",async function(){
         const currentNumber = await SimpleStorage.retrieve()
-        console.log('hey',currentNumber)
+        // console.log('hey',currentNumber)
         const expectedNumber = "0"
         assert.equal(currentNumber.toString(),expectedNumber)
     })
