b12376b51f0f4a5a2d22706616b346c9b129a78f
oladayo ahmod <oladayoahmod112@gmail.com>
2022-07-04 17:07:10 +0100

    move test file from tasks to test

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -1,14 +1,15 @@
 const {ethers} = require('hardhat')
 const {assert, expect} = require('chai')
-let SimpleStorage, SimpleStorageFactory
-describe("simple-storage", ()=>{
 
-    beforeEach(async ()=>{
+describe("SimpleStorage", ()=>{
+    let SimpleStorage, SimpleStorageFactory
+    beforeEach(async function(){
         SimpleStorage = await ethers.getContractFactory('SimpleStorage')
-        SimpleStorageFactory = SimpleStorage.deploy()
+        SimpleStorageFactory = await SimpleStorage.deploy()
     })
-    it("Current number should return 0",async ()=>{
+    it("Current number should return 0",async function(){
         const currentNumber = await SimpleStorage.retrieve()
+        console.log('hey',currentNumber)
         const expectedNumber = "0"
         assert.equal(currentNumber.toString(),expectedNumber)
     })
