042b956d21a62193e6416390d9b7f4d6e06a6dd6
oladayo ahmod <oladayoahmod112@gmail.com>
2022-07-04 17:19:41 +0100

    test get current block number

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -3,15 +3,16 @@ const {assert, expect} = require('chai')
 
 describe("SimpleStorage", ()=>{
     let SimpleStorage, SimpleStorageFactory
-    beforeEach(async function(){
+    beforeEach(async ()=>{
         SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage')
         SimpleStorage = await SimpleStorageFactory.deploy()
 
     })
-    it("Current number should return 0",async function(){
+    it("Current number should return 0",async ()=>{
         const currentNumber = await SimpleStorage.retrieve()
         // console.log('hey',currentNumber)
         const expectedNumber = "0"
         assert.equal(currentNumber.toString(),expectedNumber)
     })
+    // it("it should store number", asn)
 })
