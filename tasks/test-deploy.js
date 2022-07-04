4ac2c088969f8d76084647087a8baaa3f3f9db03
oladayo ahmod <oladayoahmod112@gmail.com>
2022-07-04 16:57:10 +0100

    test if current value is 0

--- tasks/test-deploy.js
+++ tasks/test-deploy.js
@@ -1,5 +1,5 @@
 const {ethers} = require('hardhat')
-
+const {assert, expect} = require('chai')
 let SimpleStorage, SimpleStorageFactory
 describe("simple-storage", ()=>{
 
@@ -7,5 +7,9 @@ describe("simple-storage", ()=>{
         SimpleStorage = await ethers.getContractFactory('SimpleStorage')
         SimpleStorageFactory = SimpleStorage.deploy()
     })
-    it("Current number should return 0")
+    it("Current number should return 0",async ()=>{
+        const currentNumber = await SimpleStorage.retrieve()
+        const expectedNumber = "0"
+        assert.equal(currentNumber.toString(),expectedNumber)
+    })
 })
